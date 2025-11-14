e78bc65ac5b9cea66a9680c25b992440ac0628eb
Raj Chaurasiya <rc808129@gmail.com>
2025-11-15 00:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let currentSound;
+
+function playSound(sound) {
+    if (currentSound) {
+        currentSound.pause(); // Agar koi sound play ho raha hai, toh usse stop kar do
+    }
+    currentSound = new Audio(`sounds/${sound}.mp3`); // Audio file ka path
+    currentSound.play(); // Sound play karna
+}
+
+function stopSound() {
+    if (currentSound) {
+        currentSound.pause(); // Sound ko stop karna
+        currentSound.currentTime = 0; // Sound ko reset karna
+    }
+}
